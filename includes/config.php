b38f35f0af47806d1a00d641e1b19dd6a68568c8
Slimey <34658972+Slimerino@users.noreply.github.com>
2018-08-21 15:51:41 -0400

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -6,27 +6,17 @@ session_start();
 date_default_timezone_set('Europe/London');
 
 //database credentials
-define('DBHOST','localhost');
-define('DBUSER','database username');
-define('DBPASS','password');
-define('DBNAME','database name');
+$servername = "localhost (usally)";
+$username = "(username)";
+$password = "(database password)";
+$dbname = "(database username)";
 
-//application address
-define('DIR','http://domain.com/');
-define('SITEEMAIL','noreply@domain.com');
+$DIR = "http://domain.com/";
+$SITEMAIL = "noreply@domain.com";
 
-try {
-
-	//create PDO connection
-	$db = new PDO("mysql:host=".DBHOST.";charset=utf8mb4;dbname=".DBNAME, DBUSER, DBPASS);
-    //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);//Suggested to uncomment on production websites
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Suggested to comment on production websites
-    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
-} catch(PDOException $e) {
-	//show error
-    echo '<p class="bg-danger">'.$e->getMessage().'</p>';
-    exit;
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
 //include the user class, pass in the database connection
